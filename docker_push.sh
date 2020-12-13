c769ead8efb1185bb33ec7103781034c940f0c64
kaka <kakazhang10@gmail.com>
2020-12-13 16:52:00 +0800

    fix bug

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker logout
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin docker.com
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 docker push kakazhang/django
