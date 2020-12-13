4c83ccbe61afb77b16b49a5cd8eade0ce66dd855
kaka <kakazhang10@gmail.com>
2020-12-13 15:52:29 +0800

    fix bug

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Magnifig0" | docker login -u "kakazhang" --password-stdin docker.com 
+echo "Magnifig0" | docker login -u "kakazhang" --password-stdin
 docker push kakazhang/django
